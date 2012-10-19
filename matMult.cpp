5be56dfce43e7c59dae33d0a1f70065f51b855b3
Tri Nguyen <nvutri@gmail.com>
2012-10-19 17:07:08 -0500

    Initial Commit

---
+++ matMult.cpp
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include "matMult.h"
+
+/* define macros as a neat way for index*/
+#define A(x, y) A[(x) * N + (y)]
+#define B(x, y) B[(x) * N + (y)]
+#define C(x, y) C[(x) * N + (y)]
+
+
+/*MMM function declarations */
+void matmult_ikj_a(double* A, double* B, double* C) ;
+void matmult_jik_a(double* A, double* B, double* C) ;
+
+void matmult_ikj_b(double* A, double* B, double* C) ;
+void matmult_jik_b(double* A, double* B, double* C) ;
+
+
+/* papi and matrix function declarations*/
+void init_papi();
+int begin_papi(int Event);
+long_long end_papi(int EventSet);
+void printMatrix(double* X);
+void initialize(double* const X, const double VAL);
+void flushCache(double* matrix) ;
+double* alloc(int SIZE);
+
+/*Global variable*/
+int N;
+int TYPE;
+const int EVENT = PAPI_FP_OPS;
+
+int main(int argc, char** argv) {
+
+    N = atoi(argv[1]);
+    TYPE = atoi(argv[2]);
+    /*Look for number of floating point computations*/
+
+
+    /* Run the measurement*/
+    double* A = alloc(N);
+    double* B = alloc(N);
+    double* C = alloc(N);
+
+    /*Initiallize arrays */
+    initialize(A, 1);
+    initialize(B, 2);
+    initialize(C, 0);
+
+    init_papi();
+    /*Start clocking*/
+    int eventSet = begin_papi(EVENT);
+    long long ret;
+
+    switch (TYPE) {
+        case 1:  //part a
+            matmult_ikj_a(A, B, C);
+            break;
+        case 2:  //part a
+            matmult_jik_a(A, B, C);
+            break;
+
+        case 3:  //part b
+            matmult_ikj_b(A, B, C);
+            break;
+        case 4:  //part b
+            matmult_jik_b(A, B, C);
+            break;
+    }
+
+    /* Stop  clocking    */
+    ret = end_papi(eventSet);
+    std::cout << TYPE << "\t" << N << "\t"
+              << ret  << std::endl;
+    printMatrix(C);
+
+    flushCache(A);
+    flushCache(B);
+    flushCache(C);
+
+    return 0;
+}
+
+/**
+ * Simple Matrix Multiplication.
+ * For Part A
+ */
+void matmult_ikj_a(double* A, double* B, double* C) {
+    for (unsigned i = 0; i < N; ++i) {
+        for (unsigned k = 0; k < N; ++k) {
+            for (unsigned j = 0; j < N; ++j) {
+                C(i,j) += A(i,k) * B(k,j);
+            }
+        }
+    }
+}
+
+void matmult_jik_a(double* A, double* B, double* C) {
+    for (unsigned j = 0; j < N; ++j) {
+        for (unsigned i = 0; i < N; ++i) {
+            for (unsigned k = 0; k < N; ++k) {
+                C(i,j) += A(i,k) * B(k,j);
+            }
+        }
+    }
+}
+
+/**
+ * Mini Kernel MMM for part B
+ * Unrolling loop j, i
+ * Regiser Blocking
+ */
+void matmult_ikj_b(double* A, double* B, double* C) {
+    for (unsigned i = 0; i < N; ++i) {
+        for (unsigned k = 0; k < N; ++k) {
+            register double a0 = A(i,k);
+            for (unsigned j = 0; j < N; j+=4) {
+                double* c = &C(i,j);
+                double* b = &B(k,j);
+                c[0] += a0 * b[0];
+                c[1] += a0 * b[1];
+                c[2] += a0 * b[2];
+                c[3] += a0 * b[3];
+            }
+        }
+    }
+}
+
+void matmult_jik_b(double* A, double* B, double* C) {
+    for (unsigned j = 0; j < N; ++j) {
+        for (unsigned i = 0; i < N; i+=4) {
+
+            register double c0, c1, c2, c3;
+            c0 = 0.0;
+            c1 = 0.0;
+            c2 = 0.0;
+            c3 = 0.0;
+
+            for (unsigned k = 0; k < N; ++k) {
+                register double b0;
+                b0  = B(k,   j);
+                c0 += A(i,   k) * b0;
+                c1 += A(i+1, k) * b0;
+                c2 += A(i+2, k) * b0;
+                c3 += A(i+3, k) * b0;
+            }
+            C(i,   j) += c0;
+            C(i+1, j) += c1;
+            C(i+2, j) += c2;
+            C(i+3, j) += c3;
+        }
+
+    }
+
+}
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm MATMULT
+rm matMult.o
+
+g++ -c matMult.cpp -I./papi-4.1.2.1/src
+g++ -o MATMULT matMult.o -L./papi-4.1.2.1/src/ ./papi-4.1.2.1/src/libpapi.a
+
+N=8
+TYPE=4
+
+echo -e "TYPE\tN\tPAPI_FP_OPS"
+for ((j=1; j<=TYPE; j=j+1)); do
+	./MATMULT $N $j
+done
