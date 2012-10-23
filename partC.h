749a1b626f0aa3f9251d1c25d9db9ed4bf7af1a5
Tri Nguyen <nvutri@gmail.com>
2012-10-22 22:13:54 -0500

    optimize part C

--- partC.h
+++ partC.h
@@ -24,24 +24,25 @@ void matmult_jik_c_1_4(double* A, double* B, double* C, unsigned N,
     for (unsigned j = 0; j < JB; ++j) {
         for (unsigned i = 0; i < IB; i += 4) {
             // micro kernel
+
             register double c0, c1, c2, c3;
-            c0 = 0.0;
-            c1 = 0.0;
-            c2 = 0.0;
-            c3 = 0.0;
+            c0 = c1 = c2 = c3 = 0.0;
 
             for (unsigned k = 0; k < KB; ++k) {
                 register double b0;
+                double* a = &A(i, k);
                 b0 = B(k, j);
-                c0 += A(i, k) * b0;
-                c1 += A(i + 1, k) * b0;
-                c2 += A(i + 2, k) * b0;
-                c3 += A(i + 3, k) * b0;
+                c0 += a[0] * b0;
+                c1 += a[1] * b0;
+                c2 += a[2] * b0;
+                c3 += a[3] * b0;
             }
-            C(i, j) += c0;
-            C(i + 1, j) += c1;
-            C(i + 2, j) += c2;
-            C(i + 3, j) += c3;
+            double* c = &C(i, j);
+            c[0] += c0;
+            c[1] += c1;
+            c[2] += c2;
+            c[3] += c3;
         }
     }
 }
+
